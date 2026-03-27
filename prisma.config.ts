63c966b0fcb15fb27a215017c52c225a098324f9
Nebyiou Daniel Berhane <124292845+nebioudaniel@users.noreply.github.com>
2026-03-27 20:01:04 +0300

    Update prisma.config.ts

--- prisma.config.ts
+++ prisma.config.ts
@@ -2,8 +2,12 @@ import path from "path";
 import { defineConfig } from "prisma/config";
 
 export default defineConfig({
+  // Keep the schema path so Prisma knows where your models are
   schema: path.join("prisma", "schema.prisma"),
+  
+  // Remove the datasource.url here or set it to an empty string
+  // because we are injecting the connection via the Neon Adapter in lib/db.ts
   datasource: {
-    url: process.env.DATABASE_URL, // must be set in Vercel
+    url: process.env.DATABASE_URL!, 
   },
 });
